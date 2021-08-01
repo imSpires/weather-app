340987fb87048f84bbf98c5a62a88c25c9ac5e97
Ryan Dungan <rd3@knights.ucf.edu>
2021-08-01 08:58:13 -0400

    added style to page

---
+++ assets/script.js
@@ -0,0 +1,4 @@
+var timeEl = document.getElementById("time");
+var dateEl = document.getElementById("date");
+var currentWeatherItemsEl = document.getElementById("current-weather-items");
+var timeZone = document.getElementById("time-zone");
